f03706889c5afc65f9a829462d860069f51f8c56
Gustavo Morinaga <me@gustavomorinaga.dev>
2024-01-12 00:58:46 -0300

    refactor: tab trigger suffix hover visibility

--- src/lib/layouts/rest/views/view-rest/view-rest.layout.svelte
+++ src/lib/layouts/rest/views/view-rest/view-rest.layout.svelte
@@ -65,21 +65,24 @@
 
 			<ContextMenuEditRequest requestID={tabID}>
 				<Tabs.Trigger
-					class="relative justify-between shrink-0 gap-2 min-w-52 h-12 before:absolute before:top-0 before:inset-x-0 before:h-[.125rem] data-[state=active]:before:bg-primary before:bg-transparent"
+					class="relative justify-between shrink-0 gap-2 min-w-52 h-12 px-5 group/tab-trigger before:absolute before:top-0 before:inset-x-0 before:h-[.125rem] data-[state=active]:before:bg-primary before:bg-transparent"
 					aria-label={tab.name}
 					value={tabID}
 					on:click={(event) => handleCurrentTab(event, tabID)}
 				>
 					<DialogEditRequest requestID={tabID} {form}>
-						<div class="tab-trigger-content">
+						<div class="tab-trigger-prefix">
 							<span class="method" style="color: var(--method-{methodLowCase}-color)">
 								{tab.method}
 							</span>
+						</div>
+
+						<div class="tab-trigger-content">
 							<span class="name">{tab.name}</span>
 						</div>
 					</DialogEditRequest>
 
-					<div class="tab-trigger-suffix">
+					<div class="tab-trigger-suffix invisible group-hover/tab-trigger:visible">
 						<Button
 							size="icon"
 							variant="ghost"
@@ -122,20 +125,23 @@
 </Tabs.Root>
 
 <style lang="postcss">
-	.tab-trigger-content {
-		@apply inline-flex items-baseline justify-center gap-2;
+	div.tab-trigger-prefix {
+		@apply mr-2 inline-flex items-center justify-center;
 
 		& > span.method {
 			@apply block text-xs font-medium uppercase;
 		}
+	}
+
+	div.tab-trigger-content {
+		@apply inline-flex items-baseline justify-center;
 
 		& > span.name {
 			@apply block w-32 truncate text-left text-sm font-semibold;
 		}
 	}
 
-	.tab-trigger-suffix {
-		@apply inline-flex items-center justify-center gap-2;
-		@apply opacity-60 transition-opacity hover:opacity-100;
+	div.tab-trigger-suffix {
+		@apply inline-flex items-center justify-center;
 	}
 </style>
