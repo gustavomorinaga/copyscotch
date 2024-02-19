6bdffac305e5c97b9e38c6f554576137430ea8da
Gustavo Morinaga <me@gustavomorinaga.dev>
2024-02-19 18:48:13 -0300

    style: break words when have a huge search term

--- src/lib/layouts/shared/feedbacks/feedback-not-found/feedback-not-found.layout.svelte
+++ src/lib/layouts/shared/feedbacks/feedback-not-found/feedback-not-found.layout.svelte
@@ -10,7 +10,9 @@
 
 <div class="flex select-none flex-col items-center justify-center p-4">
 	<Search class="h-6 w-6 text-muted-foreground/50" />
-	<span class="mt-2 max-w-sm whitespace-normal text-center text-tiny text-muted-foreground/50">
+	<span
+		class="mt-2 max-w-sm whitespace-normal break-words text-center text-tiny text-muted-foreground/50"
+	>
 		Nothing found for "{term}"
 	</span>
 </div>
