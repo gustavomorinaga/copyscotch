23670fc8cf87aee6083ca1cfa1d55ece6a44879f
Gustavo Morinaga <me@gustavomorinaga.dev>
2024-02-21 19:54:49 -0300

    style: make tree folder more accessible

--- src/lib/layouts/rest/trees/tree-collection/tree-folder/tree-folder.layout.svelte
+++ src/lib/layouts/rest/trees/tree-collection/tree-folder/tree-folder.layout.svelte
@@ -77,71 +77,78 @@
 </script>
 
 <Collapsible.Root class="flex shrink-0 flex-col" bind:open {onOpenChange}>
-	<Collapsible.Trigger
-		class={buttonVariants({ size: 'sm', variant: 'text', class: 'group/folder px-0' })}
-	>
-		<div
-			role="button"
-			tabindex="0"
-			class="flex flex-1 items-center justify-center"
-			on:contextmenu={(event) => {
-				event.preventDefault();
-				openOptions = true;
-			}}
-		>
-			<svelte:component this={ICONS[status]} class="mx-4 h-5 w-5" />
-			<span class="flex flex-1 py-2 pr-2">
-				<span class="truncate text-sm">{folder.name}</span>
-			</span>
-		</div>
-		<div class="flex items-center gap-2">
-			{#each OPTIONS as option}
-				<Tooltip.Root>
-					<Tooltip.Trigger asChild let:builder>
-						<Button
-							builders={[builder]}
-							size="icon"
-							variant="text"
-							class="invisible h-6 w-6 group-hover/folder:visible"
-							on:click={(event) => {
-								event.stopPropagation();
-								option.action(folder.id);
-							}}
-						>
-							<svelte:component this={option.icon} class="h-4 w-4" />
-							<span class="sr-only">{option.title}</span>
-						</Button>
-					</Tooltip.Trigger>
-					<Tooltip.Content side="top" class="select-none">
-						<span>{option.tooltip}</span>
-					</Tooltip.Content>
-				</Tooltip.Root>
-			{/each}
-
-			<DropdownMenuCollectionOptions
-				collection={folder}
-				{type}
-				bind:open={openOptions}
-				let:builder={dropdownBuilder}
+	<Collapsible.Trigger asChild let:builder>
+		<div class="group/folder flex flex-1 items-center gap-2">
+			<Button
+				builders={[builder]}
+				size="sm"
+				variant="text"
+				class="flex flex-1 items-center justify-center px-0 data-[selected=true]:text-success data-[selected=true]:hover:text-success"
 			>
-				<Tooltip.Root>
-					<Tooltip.Trigger asChild let:builder={tooltipBuilder}>
-						<Button
-							builders={[dropdownBuilder, tooltipBuilder]}
-							size="icon"
-							variant="text"
-							class="h-6 w-6"
-							on:click={(event) => event.stopPropagation()}
-						>
-							<MoreVertical class="h-4 w-4" />
-							<span class="sr-only">More options</span>
-						</Button>
-					</Tooltip.Trigger>
-					<Tooltip.Content side="top" class="select-none">
-						<span>More</span>
-					</Tooltip.Content>
-				</Tooltip.Root>
-			</DropdownMenuCollectionOptions>
+				<div
+					role="presentation"
+					class="contents"
+					on:contextmenu={(event) => {
+						event.preventDefault();
+						openOptions = true;
+					}}
+				>
+					<svelte:component this={ICONS[status]} class="mx-4 h-5 w-5" />
+					<span class="flex flex-1 py-2 pr-2">
+						<span class="truncate text-sm">{folder.name}</span>
+					</span>
+				</div>
+			</Button>
+
+			<div class="flex items-center gap-2">
+				{#each OPTIONS as option}
+					<Tooltip.Root>
+						<Tooltip.Trigger asChild let:builder>
+							<Button
+								builders={[builder]}
+								size="icon"
+								variant="text"
+								class="invisible h-6 w-6 group-hover/folder:visible"
+								on:click={(event) => {
+									event.stopPropagation();
+									option.action(folder.id);
+								}}
+							>
+								<svelte:component this={option.icon} class="h-4 w-4" />
+								<span class="sr-only">{option.title}</span>
+							</Button>
+						</Tooltip.Trigger>
+						<Tooltip.Content side="top" class="select-none">
+							<span>{option.tooltip}</span>
+						</Tooltip.Content>
+					</Tooltip.Root>
+				{/each}
+
+				<DropdownMenuCollectionOptions
+					collection={folder}
+					{type}
+					bind:open={openOptions}
+					let:builder={dropdownBuilder}
+				>
+					<Tooltip.Root>
+						<Tooltip.Trigger asChild let:builder={tooltipBuilder}>
+							<Button
+								builders={[dropdownBuilder, tooltipBuilder]}
+								size="icon"
+								variant="text"
+								class="h-6 w-6"
+								on:click={(event) => event.stopPropagation()}
+							>
+								<MoreVertical class="h-4 w-4" />
+								<span class="sr-only">More options</span>
+							</Button>
+						</Tooltip.Trigger>
+						<Tooltip.Content side="top" class="select-none">
+							<span>More</span>
+						</Tooltip.Content>
+					</Tooltip.Root>
+				</DropdownMenuCollectionOptions>
+			</div>
 		</div>
 	</Collapsible.Trigger>
 	<Collapsible.Content class="flex">
