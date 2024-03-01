b509757b7b27396019ad6ec5378bf6aa3e18883a
Gustavo Morinaga <me@gustavomorinaga.dev>
2024-02-29 21:28:34 -0300

    feat: override body content type in headers

--- src/lib/layouts/rest/toolbars/toolbar-body/toolbar-body.layout.svelte
+++ src/lib/layouts/rest/toolbars/toolbar-body/toolbar-body.layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts" context="module">
 	import { getRESTTabContext } from '$lib/contexts';
-	// import { Button } from '$lib/components/ui/button';
+	import {
+		BodyContentTypeEnum,
+		type TKeyValueInfer,
+		type TRESTRequestInfer,
+		type TRESTTabInfer
+	} from '$lib/validators';
+	import { Button } from '$lib/components/ui/button';
 	import { Separator } from '$lib/components/ui/separator';
+	import * as Tooltip from '$lib/components/ui/tooltip';
 	import * as Form from '$lib/components/ui/form';
 	import * as Select from '$lib/components/ui/select';
-	import { BodyContentTypeEnum } from '$lib/validators';
-	import type { TRESTRequestInfer, TRESTTabInfer } from '$lib/validators';
+	import RefreshCw from 'lucide-svelte/icons/refresh-cw';
 	import type { SuperForm } from 'sveltekit-superforms';
 </script>
 
@@ -20,6 +26,24 @@
 	const { options: contentTypes } = BodyContentTypeEnum;
 
 	$: ({ form: formData } = form);
+
+	function handleOverrideContentType() {
+		const updatedHeader: TKeyValueInfer = {
+			key: 'Content-Type',
+			value: $formData.body.contentType || '',
+			active: true
+		};
+		const headerIndex = $formData.headers.findIndex((h) => h.key === 'Content-Type');
+		if (headerIndex === -1) {
+			tabContext.update(tabID, { headers: [...$formData.headers, updatedHeader] });
+		} else {
+			tabContext.update(tabID, {
+				headers: $formData.headers.map((header, index) =>
+					index === headerIndex ? updatedHeader : header
+				)
+			});
+		}
+	}
 </script>
 
 <div class="sticky top-[9.825rem] z-10 flex h-10 shrink-0 flex-col bg-background lg:top-[6.825rem]">
@@ -59,6 +83,28 @@
 					<input hidden name={attrs.name} bind:value={$formData.method} />
 				</Form.Control>
 			</Form.Field>
+
+			<Tooltip.Root>
+				<Tooltip.Trigger asChild let:builder>
+					<Button
+						builders={[builder]}
+						size="sm"
+						variant="secondary"
+						aria-label="Override Content Type"
+						class="h-6"
+						on:click={handleOverrideContentType}
+					>
+						<RefreshCw class="mr-2 h-4 w-4" />
+						<span class="select-none capitalize">Override</span>
+					</Button>
+				</Tooltip.Trigger>
+				<Tooltip.Content side="top" class="select-none">
+					<span>
+						Set <code class="bg-accent px-1">Content-Type</code>
+						in Headers
+					</span>
+				</Tooltip.Content>
+			</Tooltip.Root>
 		</div>
 	</div>
 
