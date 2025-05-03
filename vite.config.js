e72cca46cf530c70f69b475a836e2d8271a997ad
Gustavo Morinaga <me@gustavomorinaga.dev>
2025-05-03 00:23:44 -0300

    feat: configure vitest for client and server testing

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import process from 'node:process';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { defineConfig } from 'vite';
 
 const isDev = process.env.NODE_ENV === 'development';
@@ -12,6 +13,31 @@ export default defineConfig({
 			'codemirror',
 			'@codemirror/lang-html',
 			'@codemirror/lang-json'
+		]
+	},
+	test: {
+		workspace: [
+			{
+				extends: './vite.config.js',
+				plugins: [svelteTesting()],
+				test: {
+					name: 'client',
+					environment: 'jsdom',
+					clearMocks: true,
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts']
+				}
+			},
+			{
+				extends: './vite.config.js',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
 		]
 	}
 });
